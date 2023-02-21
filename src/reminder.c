9af78055e2bc68486bd2a42e0edd8e1fc4066d41
ilyichpy <ilyaszuev25@gmail.com>
2023-02-21 17:26:03 +0300

    я все сломал

--- src/reminder.c
+++ src/reminder.c
@@ -1,17 +1,30 @@
 #include "functions.h"
 int main() {
-    int test_a, test_b, time_sleep;
-    char *file_name = "your_remind.txt";
-    input_remind(file_name);
-    test_a = input_time();
-    test_b = current_time();
+    int your_input = 1;
 
-    //  gcc -Wall -Werror -Wextra functions.c reminder.c -o main.out  
-    // было принято волевое решение не прятать окно терминала
-    time_sleep = test_a - test_b;
-    sleep(time_sleep);
-    FILE *fp;
-    fp = fopen(file_name, "r");
+    while (your_input == 1) {
+
+        printf("Для записи экрана введите 1, для выхода нажмите любую кнопу\n");
+        scanf("%d", &your_input);
+
+        if (your_input == 1) {
+
+            int test_a = 0, test_b = 0, time_sleep = 0;
+            char *file_name = "your_remind.txt";
+            input_remind(file_name);
+            test_a = input_time();
+            test_b = current_time();
+
+            //  gcc -Wall -Werror -Wextra functions.c reminder.c -o main.out  
+            // было принято волевое решение не прятать окно терминала
+            time_sleep = test_a - test_b;
+            sleep(time_sleep);
+            system("\nopen your_remind.txt");
+        }
+        else {
+            break;
+        }
+    }
     return 0;
 }
 
