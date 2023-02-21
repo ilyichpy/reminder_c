264e5d969a25130393cbf75def2d19700d9eb831
ilyichpy <ilyaszuev25@gmail.com>
2023-02-21 10:49:21 +0300

    1. запись удаляется из файла, это баг

---
+++ src/functions.c
@@ -0,0 +1,57 @@
+#include "functions.h"
+
+int current_time() {
+    int hours, minutes, seconds;
+
+    time_t now;
+
+    // Получить текущее время
+    // `time()` возвращает текущее время системы как значение `time_t`
+    time(&now);
+ 
+    struct tm *local = localtime(&now);
+
+    hours = local->tm_hour;         
+    minutes = local->tm_min;
+    seconds = local->tm_sec;      
+
+    return hours * 3600 + minutes * 60 + seconds;
+}
+
+void input_remind(char *name) {
+
+    char remind = '0';
+    printf("Введите напоминание:  ");
+    FILE *fp = fopen(name, "w");  //сделаем все проще, будем записывать в файл, в назначенное время будем открывать этот файл.
+
+    while (remind != '\n') {
+        scanf("%c", &remind);
+        fputc(remind, fp);
+    }
+}
+
+int input_time() {
+    int hours = 0, minutes = 0;
+    int flag = 1;
+
+     printf("\nВведите время напоминания:\n");
+    do {
+        scanf("%d", &hours);
+
+        if (hours > 24 || hours < 0) {
+            printf("\nошибка ввода\n");
+        } else {
+            printf(":");
+            scanf("%d", &minutes);
+            if (minutes > 60 || minutes < 0) {
+                printf("\nошибка ввода\n");
+            } else {
+                flag = 0;
+            }
+        }
+        
+        
+    } while (flag == 1);
+
+    return hours * 3600 + minutes * 60;
+}
