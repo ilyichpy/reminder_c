9ef8a0b928b0891762a892eb95266d845efc6376
ilyichpy <ilyaszuev25@gmail.com>
2023-02-21 14:53:16 +0300

    new version

--- src/reminder.c
+++ src/reminder.c
@@ -1,21 +1,17 @@
 #include "functions.h"
-
+#include <window.h> // для винды
 
 int main() {
     int test_a, test_b, time_sleep;
     char *file_name = "your_remind.txt";
-    // char str;
     input_remind(file_name);
-    test_a = input_time(); // идея состоит в том, чтобы эта штука возвращала (часы * 60) + минуты
-             // потом мы берем системное время и так же переводим в минуты
-             // системное время - полученное время -- время скажем так сна
-             // программа поспит нужно время, в конце откроет терминал и выведет результат!
+    test_a = input_time();
     test_b = current_time();
 
-    // printf("%d %d", test_a * 60, test_b * 60);      //  gcc -Wall -Werror -Wextra functions.c reminder.c -o main.out  
+    //  gcc -Wall -Werror -Wextra functions.c reminder.c -o main.out  
     time_sleep = test_a - test_b;
-    // FreeConsole();
-    
+    FreeConsole();//для винды
+    atexit();
     sleep(time_sleep);
     return 0;
 }
