1d17ee640681c317b1e023c1efb76cc7723f400a
ilyichpy <ilyaszuev25@gmail.com>
2023-02-22 18:13:26 +0300

    release

--- src/reminder.c
+++ src/reminder.c
@@ -5,19 +5,11 @@ int main() {
     int test_a = 0, test_b = 0, time_sleep = 0;
     char *file_name = "your_remind.txt";
 
-<<<<<<< HEAD
 
     input_remind(file_name);
     test_a = input_time(file_name);
-=======
-    test_a = input_remind(file_name);
->>>>>>> developer1
     test_b = current_time();
     time_sleep = test_a - test_b;
-<<<<<<< HEAD
-=======
-    
->>>>>>> developer1
     sleep(time_sleep);
     system("open your_remind.txt");
     
